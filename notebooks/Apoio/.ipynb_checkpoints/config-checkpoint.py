41a1131f06ca6716c54fa518a944af869f9c67fc
Antonio Manoel <antoniosouzaofs558@gmail.com>
2026-04-26 18:52:04 -0300

    Parte 4 finalizada

--- notebooks/Apoio/.ipynb_checkpoints/config-checkpoint.py
+++ notebooks/Apoio/.ipynb_checkpoints/config-checkpoint.py
@@ -4,7 +4,7 @@ from pathlib import Path
 PASTA_PROJETO = Path(__file__).resolve().parents[2]
 
 PASTA_DADOS = PASTA_PROJETO / "dados"
-PASTA_MODELOS = PASTA_PROJETO / "modelos"
+PASTA_MODELOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
 DADOS_ORIGINAIS = PASTA_DADOS / "housing.csv.zip"
