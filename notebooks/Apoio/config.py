cf8300a1d72b16eb22779bd407a7c02eecb0bd5f
Antonio Manoel <antoniosouzaofs558@gmail.com>
2026-04-26 17:02:50 -0300

    Visualizações dos melhores coeficientes do GridSearch

--- notebooks/Apoio/config.py
+++ notebooks/Apoio/config.py
@@ -4,9 +4,13 @@ from pathlib import Path
 PASTA_PROJETO = Path(__file__).resolve().parents[2]
 
 PASTA_DADOS = PASTA_PROJETO / "dados"
+PASTA_MODELOS = PASTA_PROJETO / "modelos"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
 DADOS_ORIGINAIS = PASTA_DADOS / "housing.csv.zip"
 DADOS_LIMPOS = PASTA_DADOS / "housing_clean.parquet"
 DADOS_GEO_ORIGINAIS = PASTA_DADOS / "california_counties.geojson"
 DADOS_GEO_MEDIAN = PASTA_DADOS / "gdf_counties.parquet"
+
+# coloque abaixo o caminho para os arquivos de modelos de seu projeto
+MODELO_FINAL = PASTA_MODELOS / "ridge_polyfeat_target_quantile.joblib"
